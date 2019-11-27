0a1dedc291393784ec47f8187e21f81b5dec5eab
John Hurring Jr <jhurring@google.com>
2019-11-27 11:52:42 -0500

    Added files to gcs buckets, testing plan when pushing to test branch

--- org-iam-and-policies.tf
+++ org-iam-and-policies.tf
@@ -17,19 +17,19 @@
   Org IAM
  *****************************************/
 
-//module "org_iam" {
-//  source = "github.com/john-hurringjr/test-modules/org-iam"
-//
-//  organization_admins_group = var.organization_admins_group
-//  terraform_service_account = var.terraform_service_account
-//  network_admins_group      = var.network_admins_group
-//  security_admins_group     = var.security_admins_group
-//  sre_group                 = var.sre_group
-//  domain_identity_primary   = var.domain_identity_primary
-//  domain_identity_secondary = var.domain_identity_secondary
-//  organization_id           = var.organization_id
-//
-//}
+module "org_iam" {
+  source = "github.com/john-hurringjr/test-modules/org-iam"
+
+  organization_admins_group = var.organization_admins_group
+  terraform_service_account = var.terraform_service_account
+  network_admins_group      = var.network_admins_group
+  security_admins_group     = var.security_admins_group
+  sre_group                 = var.sre_group
+  domain_identity_primary   = var.domain_identity_primary
+  domain_identity_secondary = var.domain_identity_secondary
+  organization_id           = var.organization_id
+
+}
 
 /******************************************
   Org Policies
