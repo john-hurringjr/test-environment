7d59f3005213d9b079c86dba98a998855710f3af
John Hurring Jr <jhurring@google.com>
2019-12-03 11:15:25 -0500

    Testing modules for shared vpc host vs service project

--- shared-services-project.tf
+++ shared-services-project.tf
@@ -14,37 +14,49 @@
  */
 
 /******************************************
-  "On Prem"
+  Shared VPC Host Projects
  *****************************************/
 
-//module "on_prem_project" {
-//
-//  source = "github.com/john-hurringjr/test-modules/project-creation"
-//
-//  project_friendly_name     = "On Premise Environment"
-//  unique_shared_id          = var.unique_shared_id
-//  environment               = "test"
-//  unique_project_identifier = "op-4"
-//  folder_id                 = google_folder.on_premise.id
-//  billing_account_id        = var.billing_account_id
-//  label_business_unit       = "bu1"
-//  label_restrictions        = "none"
-//  project_admin_group_id    = var.operations_admins_group
-//
-//}
-//
-module "iam_testing_3" {
+module "host_project_testing_1" {
 
-  source = "github.com/john-hurringjr/test-modules/project-creation"
+  source = "github.com/john-hurringjr/test-modules/project-creation-hostprj"
 
-  project_friendly_name     = "IAM Testing"
+  project_friendly_name     = "Shared VPC Host Testing"
   unique_shared_id          = var.unique_shared_id
   environment               = "test"
-  unique_project_identifier = "iam-3"
-  folder_id                 = google_folder.on_premise.id
+  unique_project_identifier = "host-1"
+  folder_id                 = google_folder.shared_services.id
   billing_account_id        = var.billing_account_id
   label_business_unit       = "bu1"
   label_restrictions        = "none"
-  project_admin_group_id    = var.operations_admins_group
+  network_admin_group_id    = var.network_admins_group
+  security_admin_group_id   = var.network_admins_group
 
 }
+
+
+/******************************************
+  Testing
+ *****************************************/
+
+//module "iam_testing_4" {
+//
+//  source = "github.com/john-hurringjr/test-modules/project-creation-serviceprj"
+//
+//  project_friendly_name       = "IAM Testing"
+//  unique_shared_id            = var.unique_shared_id
+//  environment                 = "test"
+//  unique_project_identifier   = "iam-3"
+//  folder_id                   = google_folder.on_premise.id
+//  billing_account_id          = var.billing_account_id
+//  label_business_unit         = "bu1"
+//  label_restrictions          = "none"
+//  project_admin_group_id      = var.operations_admins_group
+//  shared_vpc_host_project_id  = module.host_project_testing_1.outputs.project_id
+//
+//}
+
+
+/******************************************
+  "On Prem"
+ *****************************************/
