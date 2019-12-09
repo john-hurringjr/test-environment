6a39fb17014a8dc7270cd10565d86e27b653997d
John Hurring Jr <jhurring@google.com>
2019-12-09 12:47:44 -0500

    Testing billing account creation (bigquery)

--- billing-account-export.tf
+++ billing-account-export.tf
@@ -18,7 +18,7 @@
   BigQuery
  *****************************************/
 module "billing_log_sink_bigquery" {
-  source                          = "github.com/john-hurringjr/test-modules/billing-sink/bigquery"
+  source                          = "github.com/john-hurringjr/test-modules/billing-export/bigquery"
   project_id                      = module.billing_export_project.project_id
   bigquery_dataset_friendly_name  = var.billing_export_bigquery_dataset_friendly_name
   bigquery_dataset_location       = var.billing_export_bigquery_dataset_location
