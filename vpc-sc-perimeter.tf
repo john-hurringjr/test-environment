c1f6d9201ab3b2d95ed8535990667f9143245f3b
John Hurring Jr <jhurring@google.com>
2020-01-02 15:27:11 -0500

    Adding in access context manager policy (May conflict as it already exists)

--- vpc-sc-perimeter.tf
+++ vpc-sc-perimeter.tf
@@ -14,6 +14,30 @@
  */
 
 /******************************************
-  Shared VPC Host Projects
+  Acceess Context Manager Access Policy
  *****************************************/
 
+resource "google_access_context_manager_access_policy" "access-policy" {
+  parent = "organizations/${var.organization_id}"
+  title  = "access policy"
+}
+
+/******************************************
+  Acceess Context Manager Access Level
+ *****************************************/
+
+//resource "google_access_context_manager_access_level" "access-level" {
+//  parent = "accessPolicies/${google_access_context_manager_access_policy.access-policy.name}"
+//  name   = "accessPolicies/${google_access_context_manager_access_policy.access-policy.name}/accessLevels/chromeos_no_lock"
+//  title  = "chromeos_no_lock"
+//  basic {
+//    conditions {
+//      device_policy {
+//        require_screen_lock = false
+//        os_constraints {
+//          os_type = "DESKTOP_CHROME_OS"
+//        }
+//      }
+//    }
+//  }
+//}
