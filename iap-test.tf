b01050ce228cf3920778b6be0d791bcde28f50b8
John Hurring Jr <jhurring@google.com>
2020-04-02 16:25:02 -0400

    Cleaning up the iap testing

--- iap-test.tf
+++ iap-test.tf
@@ -22,41 +22,41 @@
   Isolated VPC inside Test prject (i.e. not shared host)
  *****************************************/
 
-resource "google_compute_network" "isolated_vpc_iap_testing" {
-  project                         = module.test_project_dev_1.project_id
-  name                            = "isolated-vpc"
-  routing_mode                    = "GLOBAL"
-  auto_create_subnetworks         = false
-  delete_default_routes_on_create = true
-}
-
-module "isolated_vpc_iap_testing_region_1_subnet" {
-  source                = "github.com/john-hurringjr/test-modules/networking/subnet/generic"
-  project_id            = module.test_project_dev_1.project_id
-  network_self_link     = google_compute_network.isolated_vpc_iap_testing.self_link
-  network_name          = google_compute_network.isolated_vpc_iap_testing.name
-  region                = var.region_1
-  cidr                  = var.development_vpc_region_1_cidr
-  vpc_flow_log_interval = var.shared_vpc_flow_log_interval
-  vpc_flow_log_sampling = var.shared_vpc_flow_log_sampling
-  subnet_number         = "1"
-}
-
-module "isolated_vpc_iap_testing_region_2_subnet" {
-  source                = "github.com/john-hurringjr/test-modules/networking/subnet/generic"
-  project_id            = module.test_project_dev_1.project_id
-  network_self_link     = google_compute_network.isolated_vpc_iap_testing.self_link
-  network_name          = google_compute_network.isolated_vpc_iap_testing.name
-  region                = var.region_2
-  cidr                  = var.development_vpc_region_2_cidr
-  vpc_flow_log_interval = var.shared_vpc_flow_log_interval
-  vpc_flow_log_sampling = var.shared_vpc_flow_log_sampling
-  subnet_number         = "1"
-}
-
-module "isolated_vpc_firewall_allow_iap_all" {
-  source            = "github.com/john-hurringjr/test-modules/networking/firewall-rules/all/allow-ingress-iap"
-  project_id        = module.test_project_dev_1.project_id
-  network_self_link = google_compute_network.isolated_vpc_iap_testing.self_link
-  network_name      = google_compute_network.isolated_vpc_iap_testing.name
-}
+//resource "google_compute_network" "isolated_vpc_iap_testing" {
+//  project                         = module.test_project_dev_1.project_id
+//  name                            = "isolated-vpc"
+//  routing_mode                    = "GLOBAL"
+//  auto_create_subnetworks         = false
+//  delete_default_routes_on_create = true
+//}
+//
+//module "isolated_vpc_iap_testing_region_1_subnet" {
+//  source                = "github.com/john-hurringjr/test-modules/networking/subnet/generic"
+//  project_id            = module.test_project_dev_1.project_id
+//  network_self_link     = google_compute_network.isolated_vpc_iap_testing.self_link
+//  network_name          = google_compute_network.isolated_vpc_iap_testing.name
+//  region                = var.region_1
+//  cidr                  = var.development_vpc_region_1_cidr
+//  vpc_flow_log_interval = var.shared_vpc_flow_log_interval
+//  vpc_flow_log_sampling = var.shared_vpc_flow_log_sampling
+//  subnet_number         = "1"
+//}
+//
+//module "isolated_vpc_iap_testing_region_2_subnet" {
+//  source                = "github.com/john-hurringjr/test-modules/networking/subnet/generic"
+//  project_id            = module.test_project_dev_1.project_id
+//  network_self_link     = google_compute_network.isolated_vpc_iap_testing.self_link
+//  network_name          = google_compute_network.isolated_vpc_iap_testing.name
+//  region                = var.region_2
+//  cidr                  = var.development_vpc_region_2_cidr
+//  vpc_flow_log_interval = var.shared_vpc_flow_log_interval
+//  vpc_flow_log_sampling = var.shared_vpc_flow_log_sampling
+//  subnet_number         = "1"
+//}
+//
+//module "isolated_vpc_firewall_allow_iap_all" {
+//  source            = "github.com/john-hurringjr/test-modules/networking/firewall-rules/all/allow-ingress-iap"
+//  project_id        = module.test_project_dev_1.project_id
+//  network_self_link = google_compute_network.isolated_vpc_iap_testing.self_link
+//  network_name      = google_compute_network.isolated_vpc_iap_testing.name
+//}
