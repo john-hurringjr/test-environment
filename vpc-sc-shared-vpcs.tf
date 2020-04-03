2c18d79f138981b280b5b54d97a87aef3fc08953
John Hurring Jr <jhurring@google.com>
2020-04-03 17:22:20 -0400

    Helping with route exchange and removing some resources not currently being used

--- vpc-sc-shared-vpcs.tf
+++ vpc-sc-shared-vpcs.tf
@@ -118,33 +118,33 @@ Uncomment the option you wish to test
   Option 1: Point all API calls to Restricted
  *****************************************/
 
-module "vpc_sc_dev_vpc_restricted_apis_dns" {
-  source            = "github.com/john-hurringjr/test-modules/networking/dns/internal-restricted-apis"
-  project_id        = module.vpc_sc_shared_vpc_host_project_dev.project_id
-  network_self_link = google_compute_network.vpc_sc_dev_vpc.self_link
-}
-
-module "vpc_sc_dev_vpc_restricted_apis_routing" {
-  source            = "github.com/john-hurringjr/test-modules/networking/routing/restricted-apis"
-  project_id        = module.vpc_sc_shared_vpc_host_project_dev.project_id
-  network_self_link = google_compute_network.vpc_sc_dev_vpc.self_link
-}
+//module "vpc_sc_dev_vpc_restricted_apis_dns" {
+//  source            = "github.com/john-hurringjr/test-modules/networking/dns/internal-restricted-apis"
+//  project_id        = module.vpc_sc_shared_vpc_host_project_dev.project_id
+//  network_self_link = google_compute_network.vpc_sc_dev_vpc.self_link
+//}
+//
+//module "vpc_sc_dev_vpc_restricted_apis_routing" {
+//  source            = "github.com/john-hurringjr/test-modules/networking/routing/restricted-apis"
+//  project_id        = module.vpc_sc_shared_vpc_host_project_dev.project_id
+//  network_self_link = google_compute_network.vpc_sc_dev_vpc.self_link
+//}
 
 /******************************************
   Option 2: Point all API calls to Private
  *****************************************/
 
-//module "vpc_sc_dev_vpc_private_apis_dns" {
-//  source            = "github.com/john-hurringjr/test-modules/networking/dns/internal-private-apis"
-//  project_id        = module.vpc_sc_shared_vpc_host_project_dev.project_id
-//  network_self_link = google_compute_network.vpc_sc_dev_vpc.self_link
-//}
-//
-//module "vpc_sc_dev_vpc_private_apis_routing" {
-//  source            = "github.com/john-hurringjr/test-modules/networking/routing/private-apis"
-//  project_id        = module.vpc_sc_shared_vpc_host_project_dev.project_id
-//  network_self_link = google_compute_network.vpc_sc_dev_vpc.self_link
-//}
+module "vpc_sc_dev_vpc_private_apis_dns" {
+  source            = "github.com/john-hurringjr/test-modules/networking/dns/internal-private-apis"
+  project_id        = module.vpc_sc_shared_vpc_host_project_dev.project_id
+  network_self_link = google_compute_network.vpc_sc_dev_vpc.self_link
+}
+
+module "vpc_sc_dev_vpc_private_apis_routing" {
+  source            = "github.com/john-hurringjr/test-modules/networking/routing/private-apis"
+  project_id        = module.vpc_sc_shared_vpc_host_project_dev.project_id
+  network_self_link = google_compute_network.vpc_sc_dev_vpc.self_link
+}
 
 /******************************************
   Cloud NAT - Dev - Temporary - To get dnsutils and tcpdump on image
@@ -200,52 +200,52 @@ module "vpc_sc_dev_vpc_restricted_apis_routing" {
   Shared VPC Host - Prod
  *****************************************/
 
-resource "google_compute_network" "vpc_sc_prod_vpc" {
-  depends_on                      = [module.vpc_sc_shared_vpc_host_project_prod]
-  project                         = module.vpc_sc_shared_vpc_host_project_prod.project_id
-  name                            = "vpc-sc-prod-vpc"
-  routing_mode                    = "GLOBAL"
-  auto_create_subnetworks         = false
-  delete_default_routes_on_create = true
-}
-
-module "vpc_sc_prod_vpc_region_1_subnet_1" {
-  source                = "github.com/john-hurringjr/test-modules/networking/subnet/generic"
-  project_id            = module.vpc_sc_shared_vpc_host_project_prod.project_id
-  network_self_link     = google_compute_network.vpc_sc_prod_vpc.self_link
-  network_name          = google_compute_network.vpc_sc_prod_vpc.name
-  region                = var.region_1
-  cidr                  = var.production_vpc_region_1_cidr
-  vpc_flow_log_interval = var.shared_vpc_flow_log_interval
-  vpc_flow_log_sampling = var.shared_vpc_flow_log_sampling
-  subnet_number         = "1"
-}
-
-module "vpc_sc_prod_vpc_region_2_subnet_1" {
-  source                = "github.com/john-hurringjr/test-modules/networking/subnet/generic"
-  project_id            = module.vpc_sc_shared_vpc_host_project_prod.project_id
-  network_self_link     = google_compute_network.vpc_sc_prod_vpc.self_link
-  network_name          = google_compute_network.vpc_sc_prod_vpc.name
-  region                = var.region_2
-  cidr                  = var.production_vpc_region_2_cidr
-  vpc_flow_log_interval = var.shared_vpc_flow_log_interval
-  vpc_flow_log_sampling = var.shared_vpc_flow_log_sampling
-  subnet_number         = "1"
-}
-
-module "vpc_sc_prod_vpc_firewall_allow_iap_all" {
-  source            = "github.com/john-hurringjr/test-modules/networking/firewall-rules/all/allow-ingress-iap"
-  project_id        = module.vpc_sc_shared_vpc_host_project_prod.project_id
-  network_self_link = google_compute_network.vpc_sc_prod_vpc.self_link
-  network_name      = google_compute_network.vpc_sc_prod_vpc.name
-}
-
-module "vpc_sc_prod_vpc_firewall_allow_rfc1918_all" {
-  source            = "github.com/john-hurringjr/test-modules/networking/firewall-rules/all/allow-ingress-rfc1918"
-  project_id        = module.vpc_sc_shared_vpc_host_project_prod.project_id
-  network_self_link = google_compute_network.vpc_sc_prod_vpc.self_link
-  network_name      = google_compute_network.vpc_sc_prod_vpc.name
-}
+//resource "google_compute_network" "vpc_sc_prod_vpc" {
+//  depends_on                      = [module.vpc_sc_shared_vpc_host_project_prod]
+//  project                         = module.vpc_sc_shared_vpc_host_project_prod.project_id
+//  name                            = "vpc-sc-prod-vpc"
+//  routing_mode                    = "GLOBAL"
+//  auto_create_subnetworks         = false
+//  delete_default_routes_on_create = true
+//}
+//
+//module "vpc_sc_prod_vpc_region_1_subnet_1" {
+//  source                = "github.com/john-hurringjr/test-modules/networking/subnet/generic"
+//  project_id            = module.vpc_sc_shared_vpc_host_project_prod.project_id
+//  network_self_link     = google_compute_network.vpc_sc_prod_vpc.self_link
+//  network_name          = google_compute_network.vpc_sc_prod_vpc.name
+//  region                = var.region_1
+//  cidr                  = var.production_vpc_region_1_cidr
+//  vpc_flow_log_interval = var.shared_vpc_flow_log_interval
+//  vpc_flow_log_sampling = var.shared_vpc_flow_log_sampling
+//  subnet_number         = "1"
+//}
+//
+//module "vpc_sc_prod_vpc_region_2_subnet_1" {
+//  source                = "github.com/john-hurringjr/test-modules/networking/subnet/generic"
+//  project_id            = module.vpc_sc_shared_vpc_host_project_prod.project_id
+//  network_self_link     = google_compute_network.vpc_sc_prod_vpc.self_link
+//  network_name          = google_compute_network.vpc_sc_prod_vpc.name
+//  region                = var.region_2
+//  cidr                  = var.production_vpc_region_2_cidr
+//  vpc_flow_log_interval = var.shared_vpc_flow_log_interval
+//  vpc_flow_log_sampling = var.shared_vpc_flow_log_sampling
+//  subnet_number         = "1"
+//}
+//
+//module "vpc_sc_prod_vpc_firewall_allow_iap_all" {
+//  source            = "github.com/john-hurringjr/test-modules/networking/firewall-rules/all/allow-ingress-iap"
+//  project_id        = module.vpc_sc_shared_vpc_host_project_prod.project_id
+//  network_self_link = google_compute_network.vpc_sc_prod_vpc.self_link
+//  network_name      = google_compute_network.vpc_sc_prod_vpc.name
+//}
+//
+//module "vpc_sc_prod_vpc_firewall_allow_rfc1918_all" {
+//  source            = "github.com/john-hurringjr/test-modules/networking/firewall-rules/all/allow-ingress-rfc1918"
+//  project_id        = module.vpc_sc_shared_vpc_host_project_prod.project_id
+//  network_self_link = google_compute_network.vpc_sc_prod_vpc.self_link
+//  network_name      = google_compute_network.vpc_sc_prod_vpc.name
+//}
 
 
 /******************************************
@@ -258,18 +258,18 @@ Uncomment the option you wish to test
   Option 1: Point all API calls to Restricted
  *****************************************/
 
-
-module "vpc_sc_prod_vpc_restricted_apis_dns" {
-  source            = "github.com/john-hurringjr/test-modules/networking/dns/internal-restricted-apis"
-  project_id        = module.vpc_sc_shared_vpc_host_project_prod.project_id
-  network_self_link = google_compute_network.vpc_sc_prod_vpc.self_link
-}
-
-module "vpc_sc_prod_vpc_restricted_apis_routing" {
-  source            = "github.com/john-hurringjr/test-modules/networking/routing/restricted-apis"
-  project_id        = module.vpc_sc_shared_vpc_host_project_prod.project_id
-  network_self_link = google_compute_network.vpc_sc_prod_vpc.self_link
-}
+//
+//module "vpc_sc_prod_vpc_restricted_apis_dns" {
+//  source            = "github.com/john-hurringjr/test-modules/networking/dns/internal-restricted-apis"
+//  project_id        = module.vpc_sc_shared_vpc_host_project_prod.project_id
+//  network_self_link = google_compute_network.vpc_sc_prod_vpc.self_link
+//}
+//
+//module "vpc_sc_prod_vpc_restricted_apis_routing" {
+//  source            = "github.com/john-hurringjr/test-modules/networking/routing/restricted-apis"
+//  project_id        = module.vpc_sc_shared_vpc_host_project_prod.project_id
+//  network_self_link = google_compute_network.vpc_sc_prod_vpc.self_link
+//}
 
 /******************************************
   Option 2: Point all API calls to Private
