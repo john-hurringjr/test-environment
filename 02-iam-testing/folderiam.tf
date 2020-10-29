1fc212688c42dd9bc3b7e40c0534626f20674845
John Hurring Jr <jhurring@google.com>
2020-10-29 10:28:14 -0400

    Testing permissions at folder level. Removed - Org viewer

--- 02-iam-testing/folderiam.tf
+++ 02-iam-testing/folderiam.tf
@@ -52,13 +52,6 @@ data "google_iam_policy" "org_perm_folder_policy_data" {
     members = [
       "serviceAccount:${var.terraform_service_account}",
     ]
-    role = "roles/resourcemanager.organizationViewer"
-  }
-
-  binding {
-    members = [
-      "serviceAccount:${var.terraform_service_account}",
-    ]
     role = "roles/securitycenter.admin"
   }
 
