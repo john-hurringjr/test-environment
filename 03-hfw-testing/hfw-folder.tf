595bc6c5915911d59efeaae37d299832b8e92476
John Hurring Jr <jhurring@google.com>
2020-12-29 10:16:06 -0500

    Adding more fw rules to hfw list for folder. Creating project which will be used to test creating vpc with these rules applied

--- 03-hfw-testing/hfw-folder.tf
+++ 03-hfw-testing/hfw-folder.tf
@@ -23,7 +23,77 @@ resource "google_compute_organization_security_policy" "test_folder_hfw_policy_1
   parent       = google_folder.hfw_testing_top_level.id
 }
 
-resource "google_compute_organization_security_policy_rule" "test_folder_hfw_policy_1_rule_1" {
+resource "google_compute_organization_security_policy_rule" "test_folder_hfw_policy_iap_ssh_next" {
+  provider = google-beta
+
+  policy_id = google_compute_organization_security_policy.test_folder_hfw_policy_1.id
+  action = "goto_next"
+
+  direction = "INGRESS"
+
+  match {
+    config {
+      src_ip_ranges = ["35.235.240.0/20"]
+      layer4_config {
+        ip_protocol = "tcp"
+        port        = "22"
+      }
+    }
+  }
+
+  priority = 50
+
+}
+
+resource "google_compute_organization_security_policy_rule" "test_folder_hfw_policy_rfcingress_next" {
+  provider = google-beta
+
+  policy_id = google_compute_organization_security_policy.test_folder_hfw_policy_1.id
+  action = "goto_next"
+
+  direction = "INGRESS"
+
+  match {
+    config {
+      src_ip_ranges = ["10.0.0.0/8", "192.168.0.0/16", "172.16.0.0/12"]
+      layer4_config {
+        ip_protocol = "tcp"
+      }
+      layer4_config {
+        ip_protocol = "udp"
+      }
+    }
+  }
+
+  priority = 90
+
+}
+
+resource "google_compute_organization_security_policy_rule" "test_folder_hfw_policy_rfcengress_next" {
+  provider = google-beta
+
+  policy_id = google_compute_organization_security_policy.test_folder_hfw_policy_1.id
+  action = "goto_next"
+
+  direction = "ENGRESS"
+
+  match {
+    config {
+      src_ip_ranges = ["10.0.0.0/8", "192.168.0.0/16", "172.16.0.0/12"]
+      layer4_config {
+        ip_protocol = "tcp"
+      }
+      layer4_config {
+        ip_protocol = "udp"
+      }
+    }
+  }
+
+  priority = 80
+
+}
+
+resource "google_compute_organization_security_policy_rule" "test_folder_hfw_policy_allingress_deny" {
   provider = google-beta
 
   policy_id = google_compute_organization_security_policy.test_folder_hfw_policy_1.id
@@ -46,6 +116,32 @@ resource "google_compute_organization_security_policy_rule" "test_folder_hfw_pol
   priority = 100
 
 }
+
+resource "google_compute_organization_security_policy_rule" "test_folder_hfw_policy_allengress_deny" {
+  provider = google-beta
+
+  policy_id = google_compute_organization_security_policy.test_folder_hfw_policy_1.id
+  action = "deny"
+
+  direction = "ENGRESS"
+
+  match {
+    config {
+      src_ip_ranges = ["0.0.0.0/0"]
+      layer4_config {
+        ip_protocol = "tcp"
+      }
+      layer4_config {
+        ip_protocol = "udp"
+      }
+    }
+  }
+
+  priority = 110
+
+}
+
+
 
 resource "google_compute_organization_security_policy_association" "test_folder_hfw_policy_1_applied" {
   provider = google-beta
