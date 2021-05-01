893cd34945a5ae565da5e25c28c0a3c83d5cf201
John Hurring Jr <jhurring@google.com>
2021-05-01 16:41:17 -0400

    Testing out ingress and egress

--- 06-ingress-egress/shared-service-host-project-vpc.tf
+++ 06-ingress-egress/shared-service-host-project-vpc.tf
@@ -18,95 +18,95 @@
   Shared VPC Host
  *****************************************/
 
-module "shared_vpc_host_project" {
-  source                    = "github.com/john-hurringjr/test-modules/project-creation/vpc-sc-restricted-access-2/shared-vpc-host"
-  project_friendly_name     = "Shared VPC Host"
-  unique_shared_id          = var.project_unique_shared_id
-  environment               = "srvs"
-  folder_id                 = google_folder.ingress_egress.id
-  billing_account_id        = var.billing_account_id
-  label_business_unit       = "shared-services"
-  label_restrictions        = "none"
-  network_viewer_group_id   = var.network_admins_group
-  service_perimeter_name    = google_access_context_manager_service_perimeter.service_perimeter_configuration_shared_srvs.name
-}
+//module "shared_vpc_host_project" {
+//  source                    = "github.com/john-hurringjr/test-modules/project-creation/vpc-sc-restricted-access-2/shared-vpc-host"
+//  project_friendly_name     = "Shared VPC Host"
+//  unique_shared_id          = var.project_unique_shared_id
+//  environment               = "srvs"
+//  folder_id                 = google_folder.ingress_egress.id
+//  billing_account_id        = var.billing_account_id
+//  label_business_unit       = "shared-services"
+//  label_restrictions        = "none"
+//  network_viewer_group_id   = var.network_admins_group
+//  service_perimeter_name    = google_access_context_manager_service_perimeter.service_perimeter_configuration_shared_srvs.name
+//}
 
 /******************************************
   Shared - VPC and Subnets
  *****************************************/
-# Network
-resource "google_compute_network" "shared_project_vpc" {
-  project                         = module.shared_vpc_host_project.project_id
-  name                            = "shared-vpc"
-  routing_mode                    = "GLOBAL"
-  auto_create_subnetworks         = false
-  delete_default_routes_on_create = true
-}
-
-# Subnets
-module "shared_vpc_subnet_1" {
-  source                  = "github.com/john-hurringjr/test-modules/networking/subnet/gke"
-  project_id              = module.shared_vpc_host_project.project_id
-  network_self_link       = google_compute_network.shared_project_vpc.self_link
-  network_name            = google_compute_network.shared_project_vpc.name
-  region                  = var.region
-  primary_cidr            = "10.0.0.0/19"
-  alias_gke_pod_cidr      = "10.2.0.0/18"
-  alias_gke_service_cidr  = "10.1.0.0/19"
-  vpc_flow_log_interval   = "INTERVAL_5_MIN"
-  vpc_flow_log_sampling   = 0.8
-  subnet_number           = "1"
-}
-
-/******************************************
-  Shared - DNS & Routing
- *****************************************/
-module "shared_vpc_restricted_apis_dns" {
-  source            = "github.com/john-hurringjr/test-modules/networking/dns/internal-restricted-apis"
-  project_id        = module.shared_vpc_host_project.project_id
-  network_self_link = google_compute_network.shared_project_vpc.self_link
-}
-
-module "shared_vpc_restricted_apis_routing" {
-  source            = "github.com/john-hurringjr/test-modules/networking/routing/restricted-apis"
-  project_id        = module.shared_vpc_host_project.project_id
-  network_self_link = google_compute_network.shared_project_vpc.self_link
-}
-
-/******************************************
-  Shared - Firewalls
- *****************************************/
-# Allows ingress on 22, 3389, 443 on all VMs from all rfc1918
-module "shared_vpc_firewall_allow_ingress_rfc1918_limited" {
-  source            = "github.com/john-hurringjr/test-modules/networking/firewall-rules/all/allow-ingress-rfc1918-limited"
-  project_id        = module.shared_vpc_host_project.project_id
-  network_self_link = google_compute_network.shared_project_vpc.self_link
-  network_name      = google_compute_network.shared_project_vpc.name
-}
-
-module "shared_vpc_prod_firewall_allow_iap_all" {
-  source            = "github.com/john-hurringjr/test-modules/networking/firewall-rules/all/allow-ingress-iap"
-  project_id        = module.shared_vpc_host_project.project_id
-  network_self_link = google_compute_network.shared_project_vpc.self_link
-  network_name      = google_compute_network.shared_project_vpc.name
-}
-
-/******************************************
-  Shared Cloud NAT
- *****************************************/
-module "shared_nat" {
-  source                  = "github.com/john-hurringjr/test-modules//networking/nat/auto-ip-all-region-subnets?ref=master"
-  project_id              = module.shared_vpc_host_project.project_id
-  network_self_link       = google_compute_network.shared_project_vpc.self_link
-  network_name            = google_compute_network.shared_project_vpc.name
-  cloud_router_asn_number = 4200000500
-  nat_region              = var.region
-}
-
-resource "google_compute_route" "default_shared_route" {
-  project           = module.shared_vpc_host_project.project_id
-  dest_range        = "0.0.0.0/0"
-  name              = "shared-default"
-  network           = google_compute_network.shared_project_vpc.name
-  next_hop_gateway  = "default-internet-gateway"
-}
+//# Network
+//resource "google_compute_network" "shared_project_vpc" {
+//  project                         = module.shared_vpc_host_project.project_id
+//  name                            = "shared-vpc"
+//  routing_mode                    = "GLOBAL"
+//  auto_create_subnetworks         = false
+//  delete_default_routes_on_create = true
+//}
+//
+//# Subnets
+//module "shared_vpc_subnet_1" {
+//  source                  = "github.com/john-hurringjr/test-modules/networking/subnet/gke"
+//  project_id              = module.shared_vpc_host_project.project_id
+//  network_self_link       = google_compute_network.shared_project_vpc.self_link
+//  network_name            = google_compute_network.shared_project_vpc.name
+//  region                  = var.region
+//  primary_cidr            = "10.0.0.0/19"
+//  alias_gke_pod_cidr      = "10.2.0.0/18"
+//  alias_gke_service_cidr  = "10.1.0.0/19"
+//  vpc_flow_log_interval   = "INTERVAL_5_MIN"
+//  vpc_flow_log_sampling   = 0.8
+//  subnet_number           = "1"
+//}
+//
+///******************************************
+//  Shared - DNS & Routing
+// *****************************************/
+//module "shared_vpc_restricted_apis_dns" {
+//  source            = "github.com/john-hurringjr/test-modules/networking/dns/internal-restricted-apis"
+//  project_id        = module.shared_vpc_host_project.project_id
+//  network_self_link = google_compute_network.shared_project_vpc.self_link
+//}
+//
+//module "shared_vpc_restricted_apis_routing" {
+//  source            = "github.com/john-hurringjr/test-modules/networking/routing/restricted-apis"
+//  project_id        = module.shared_vpc_host_project.project_id
+//  network_self_link = google_compute_network.shared_project_vpc.self_link
+//}
+//
+///******************************************
+//  Shared - Firewalls
+// *****************************************/
+//# Allows ingress on 22, 3389, 443 on all VMs from all rfc1918
+//module "shared_vpc_firewall_allow_ingress_rfc1918_limited" {
+//  source            = "github.com/john-hurringjr/test-modules/networking/firewall-rules/all/allow-ingress-rfc1918-limited"
+//  project_id        = module.shared_vpc_host_project.project_id
+//  network_self_link = google_compute_network.shared_project_vpc.self_link
+//  network_name      = google_compute_network.shared_project_vpc.name
+//}
+//
+//module "shared_vpc_prod_firewall_allow_iap_all" {
+//  source            = "github.com/john-hurringjr/test-modules/networking/firewall-rules/all/allow-ingress-iap"
+//  project_id        = module.shared_vpc_host_project.project_id
+//  network_self_link = google_compute_network.shared_project_vpc.self_link
+//  network_name      = google_compute_network.shared_project_vpc.name
+//}
+//
+///******************************************
+//  Shared Cloud NAT
+// *****************************************/
+//module "shared_nat" {
+//  source                  = "github.com/john-hurringjr/test-modules//networking/nat/auto-ip-all-region-subnets?ref=master"
+//  project_id              = module.shared_vpc_host_project.project_id
+//  network_self_link       = google_compute_network.shared_project_vpc.self_link
+//  network_name            = google_compute_network.shared_project_vpc.name
+//  cloud_router_asn_number = 4200000500
+//  nat_region              = var.region
+//}
+//
+//resource "google_compute_route" "default_shared_route" {
+//  project           = module.shared_vpc_host_project.project_id
+//  dest_range        = "0.0.0.0/0"
+//  name              = "shared-default"
+//  network           = google_compute_network.shared_project_vpc.name
+//  next_hop_gateway  = "default-internet-gateway"
+//}
--- 06-ingress-egress/target-projects.tf
+++ 06-ingress-egress/target-projects.tf
@@ -17,49 +17,49 @@
   Target Project
  *****************************************/
 
-module "target_project" {
-  source                    = "github.com/john-hurringjr/test-modules/project-creation/vpc-sc-restricted-access-2/shared-vpc-host"
-  project_friendly_name     = "Target Project"
-  unique_shared_id          = var.project_unique_shared_id
-  environment               = "tar"
-  folder_id                 = google_folder.ingress_egress.id
-  billing_account_id        = var.billing_account_id
-  label_business_unit       = "target"
-  label_restrictions        = "none"
-  network_viewer_group_id   = var.network_admins_group
-  service_perimeter_name    = google_access_context_manager_service_perimeter.service_perimeter_configuration_target.name
-}
-
-/******************************************
-  Service Prj (Attached to Shared Host)
- *****************************************/
-module "shared_vpc_service_project" {
-  source                    = "github.com/john-hurringjr/test-modules/project-creation/vpc-sc-restricted-access-2/shared-vpc-service"
-  project_friendly_name       = "Shared Perimeter - Service"
-  unique_shared_id            = var.project_unique_shared_id
-  environment                 = "srvs"
-  folder_id                   = google_folder.ingress_egress.id
-  billing_account_id          = var.billing_account_id
-  unique_project_identifier   = "shrd-srvs-prj"
-  project_viewer_group        = var.network_admins_group
-  service_perimeter_name    = google_access_context_manager_service_perimeter.service_perimeter_configuration_target.name
-  shared_vpc_host_project_id  = module.shared_vpc_host_project.project_id
-}
-
-/******************************************
-  Service Account
- *****************************************/
-
-resource "google_service_account" "terraform_test_service_account_1" {
-  project     = module.target_project.project_id
-  account_id  = "terraform-test-1"
-}
-
-/******************************************
-  Service Account
- *****************************************/
-
-resource "google_service_account" "git_service_account_1" {
-  project     = module.shared_vpc_service_project.project_id
-  account_id  = "git-test-1"
-}
+//module "target_project" {
+//  source                    = "github.com/john-hurringjr/test-modules/project-creation/vpc-sc-restricted-access-2/shared-vpc-host"
+//  project_friendly_name     = "Target Project"
+//  unique_shared_id          = var.project_unique_shared_id
+//  environment               = "tar"
+//  folder_id                 = google_folder.ingress_egress.id
+//  billing_account_id        = var.billing_account_id
+//  label_business_unit       = "target"
+//  label_restrictions        = "none"
+//  network_viewer_group_id   = var.network_admins_group
+//  service_perimeter_name    = google_access_context_manager_service_perimeter.service_perimeter_configuration_target.name
+//}
+//
+///******************************************
+//  Service Prj (Attached to Shared Host)
+// *****************************************/
+//module "shared_vpc_service_project" {
+//  source                    = "github.com/john-hurringjr/test-modules/project-creation/vpc-sc-restricted-access-2/shared-vpc-service"
+//  project_friendly_name       = "Shared Perimeter - Service"
+//  unique_shared_id            = var.project_unique_shared_id
+//  environment                 = "srvs"
+//  folder_id                   = google_folder.ingress_egress.id
+//  billing_account_id          = var.billing_account_id
+//  unique_project_identifier   = "shrd-srvs-prj"
+//  project_viewer_group        = var.network_admins_group
+//  service_perimeter_name    = google_access_context_manager_service_perimeter.service_perimeter_configuration_target.name
+//  shared_vpc_host_project_id  = module.shared_vpc_host_project.project_id
+//}
+//
+///******************************************
+//  Service Account
+// *****************************************/
+//
+//resource "google_service_account" "terraform_test_service_account_1" {
+//  project     = module.target_project.project_id
+//  account_id  = "terraform-test-1"
+//}
+//
+///******************************************
+//  Service Account
+// *****************************************/
+//
+//resource "google_service_account" "git_service_account_1" {
+//  project     = module.shared_vpc_service_project.project_id
+//  account_id  = "git-test-1"
+//}
