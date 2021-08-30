b501184b780eb4a7b0654fcaddebb9f5e9f7cf5e
John Hurring Jr <jhurring@google.com>
2021-08-30 17:07:46 -0400

    Trying two blocks of limited IAM binding

--- 02-iam-testing/limit-iam-binding.tf
+++ 02-iam-testing/limit-iam-binding.tf
@@ -41,7 +41,7 @@ resource "google_project_iam_policy" "limited_iam_binding_policy" {
   project     = google_project.limit_iam_binding_1.project_id
 }
 
-resource "google_project_iam_binding" "test_limited_iam_admin_permissions_1" {
+resource "googleprojiammem" "test_limited_iam_admin_permissions_1" {
   project = google_project.limit_iam_binding_1.project_id
   members = [
     "serviceAccount:${google_service_account.limited_iam_or_test.email}",
@@ -64,7 +64,7 @@ resource "google_project_iam_binding" "test_limited_iam_admin_permissions_2" {
   role = "roles/resourcemanager.projectIamAdmin"
 
   condition {
-    title       = "Test to limit IAM bindings permissions - First 130"
+    title       = "Test to limit IAM bindings permissions - Second 130"
     expression  = "api.getAttribute(api.getAttribute('iam.googleapis.com/modifiedGrantsByRole', []).hasOnly(['roles/cloudbuild.builds.builder', 'roles/cloudbuild.builds.editor', 'roles/cloudbuild.builds.viewer'])"
   }
 }
