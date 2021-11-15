113783c67336703b27e32b14393e242abd1e123d
John Hurring Jr <jhurring@google.com>
2021-11-15 18:57:48 -0500

    Shutting Down

--- 02-iam-testing/folders.tf
+++ 02-iam-testing/folders.tf
@@ -1,74 +1,74 @@
-/**
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/******************************************
-  Folders
- *****************************************/
-
-resource "google_folder" "iam_top_level" {
-  display_name  = "IAM Testing"
-  parent        = "organizations/${var.organization_id}"
-}
-
-resource "google_folder" "no_domain_restricted_sharing" {
-  display_name  = "No Domain Restricted Sharing"
-  parent        = google_folder.iam_top_level.id
-}
-
-resource "google_folder" "empty_iam_projects" {
-  display_name  = "Empty IAM Projects"
-  parent        = google_folder.iam_top_level.id
-}
-
-resource "google_folder" "conditional_access_testing" {
-  display_name  = "Conditional Access Testing"
-  parent        = google_folder.iam_top_level.id
-}
-
-resource "google_folder" "service_agent_testing_projects" {
-  display_name  = "Service Agent Projects"
-  parent        = google_folder.iam_top_level.id
-}
-
-resource "google_folder" "org_perm_folder_testing" {
-  display_name  = "Org Permissions"
-  parent        = google_folder.iam_top_level.id
-}
-
-resource "google_folder" "scc_folder_perm_testing" {
-  display_name  = "Security Command Center"
-  parent        = google_folder.iam_top_level.id
-}
-
-resource "google_folder" "testing_iam_1_folder" {
-  display_name  = var.testing_iam_1_folder
-  parent        = google_folder.iam_top_level.id
-}
-
-resource "google_folder" "testing_iam_2_folder" {
-  display_name  = var.testing_iam_2_folder
-  parent        = google_folder.iam_top_level.id
-}
-
-resource "google_folder" "testing_iam_blank_folder" {
-  display_name  = "Testing IAM Blank"
-  parent        = google_folder.iam_top_level.id
-}
-
-
-resource "google_folder" "testing_iam_api_keys" {
-  display_name  = "Testing API Keys"
-  parent        = google_folder.iam_top_level.id
-}
+#/**
+# *
+# * Licensed under the Apache License, Version 2.0 (the "License");
+# * you may not use this file except in compliance with the License.
+# * You may obtain a copy of the License at
+# *
+# *      http://www.apache.org/licenses/LICENSE-2.0
+# *
+# * Unless required by applicable law or agreed to in writing, software
+# * distributed under the License is distributed on an "AS IS" BASIS,
+# * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# * See the License for the specific language governing permissions and
+# * limitations under the License.
+# */
+#
+#/******************************************
+#  Folders
+# *****************************************/
+#
+#resource "google_folder" "iam_top_level" {
+#  display_name  = "IAM Testing"
+#  parent        = "organizations/${var.organization_id}"
+#}
+#
+#resource "google_folder" "no_domain_restricted_sharing" {
+#  display_name  = "No Domain Restricted Sharing"
+#  parent        = google_folder.iam_top_level.id
+#}
+#
+#resource "google_folder" "empty_iam_projects" {
+#  display_name  = "Empty IAM Projects"
+#  parent        = google_folder.iam_top_level.id
+#}
+#
+#resource "google_folder" "conditional_access_testing" {
+#  display_name  = "Conditional Access Testing"
+#  parent        = google_folder.iam_top_level.id
+#}
+#
+#resource "google_folder" "service_agent_testing_projects" {
+#  display_name  = "Service Agent Projects"
+#  parent        = google_folder.iam_top_level.id
+#}
+#
+#resource "google_folder" "org_perm_folder_testing" {
+#  display_name  = "Org Permissions"
+#  parent        = google_folder.iam_top_level.id
+#}
+#
+#resource "google_folder" "scc_folder_perm_testing" {
+#  display_name  = "Security Command Center"
+#  parent        = google_folder.iam_top_level.id
+#}
+#
+#resource "google_folder" "testing_iam_1_folder" {
+#  display_name  = var.testing_iam_1_folder
+#  parent        = google_folder.iam_top_level.id
+#}
+#
+#resource "google_folder" "testing_iam_2_folder" {
+#  display_name  = var.testing_iam_2_folder
+#  parent        = google_folder.iam_top_level.id
+#}
+#
+#resource "google_folder" "testing_iam_blank_folder" {
+#  display_name  = "Testing IAM Blank"
+#  parent        = google_folder.iam_top_level.id
+#}
+#
+#
+#resource "google_folder" "testing_iam_api_keys" {
+#  display_name  = "Testing API Keys"
+#  parent        = google_folder.iam_top_level.id
+#}
--- 02-iam-testing/limit-iam-binding.tf
+++ 02-iam-testing/limit-iam-binding.tf
@@ -1,122 +1,122 @@
-/**
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/******************************************
-  Project
- *****************************************/
-
-resource "google_project" "limit_iam_binding_1" {
-  name            = "${var.project_unique_shared_id}-limit-iam-binding"
-  project_id      = "${var.project_unique_shared_id}-limit-iam-binding"
-  folder_id       = google_folder.conditional_access_testing.id
-  billing_account = var.billing_account_id
-}
-
-resource "google_project" "limit_iam_binding_2" {
-  name            = "${var.project_unique_shared_id}-limit-iam-binding"
-  project_id      = "${var.project_unique_shared_id}-limit-iam-binding-2"
-  folder_id       = google_folder.conditional_access_testing.id
-  billing_account = var.billing_account_id
-}
-
-
-/******************************************
-  IAM Policy
- *****************************************/
-
-
-resource "google_project_iam_member" "test_limited_iam_admin_permissions_1" {
-  project = google_project.limit_iam_binding_1.project_id
-  member = "group:${var.limited_iam_bindings_group}"
-  role = "roles/resourcemanager.projectIamAdmin"
-  condition {
-    title       = "Test to limit IAM bindings permissions - First 130"
-    expression  = "api.getAttribute('iam.googleapis.com/modifiedGrantsByRole', []).hasOnly(['roles/storage.admin', 'roles/bigquery.admin', 'roles/bigtable.admin', 'projects/iamtest-limit-iam-binding/roles/LotsORoles'])"
-  }
-}
-
-resource "google_project_iam_member" "test_limited_iam_admin_permissions_2" {
-  project = google_project.limit_iam_binding_1.project_id
-  member = "group:${var.limited_iam_bindings_group}"
-  role = "roles/resourcemanager.projectIamAdmin"
-  condition {
-    title       = "Test to limit IAM bindings permissions - Second 130"
-    expression  = "api.getAttribute('iam.googleapis.com/modifiedGrantsByRole', []).hasOnly(['roles/cloudbuild.builds.builder', 'roles/cloudbuild.builds.editor', 'roles/cloudbuild.builds.viewer'])"
-  }
-}
-
-
-
-
-
-
-
-//resource "google_folder_iam_binding" "test_limited_iam_admin_permissions" {
-//    folder = google_folder.conditional_access_testing.id
-//    members = [
-//      "group:${var.limited_iam_bindings_group}",
-//    ]
-//    role = "roles/resourcemanager.projectIamAdmin"
-//
-//    condition {
-//      title       = "Test to limit IAM bindings permissions"
-//      expression  = "api.getAttribute('iam.googleapis.com/modifiedGrantsByRole', []).hasOnly(['roles/storage.admin', 'roles/billing.user', 'roles/bigquery.admin', 'roles/bigtable.admin']) || xyz " #Can OR it up to 12 times (13 functions to call)
-//    }
-//
-//}
-
-
-resource "google_service_account" "limited_iam_or_test" {
-  project = google_project.limit_iam_binding_1.project_id
-  account_id = "limited-iam-or-test"
-}
-
-resource "google_folder_iam_binding" "test_limited_iam_admin_permissions" {
-  folder = google_folder.conditional_access_testing.id
-  members = [
-    "group:${var.limited_iam_bindings_group}",
-  ]
-  role = "roles/resourcemanager.projectIamAdmin"
-
-  condition {
-    title       = "Test to limit IAM bindings permissions"
-    expression  ="api.getAttribute('iam.googleapis.com/modifiedGrantsByRole', []).hasOnly(['roles/storage.admin', 'roles/billing.user','roles/bigquery.admin', 'roles/bigtable.admin'])" #Can OR it up to 12 times (13 functions to call)
-  }
-
-}
-
-//
-//
-///******************************************
-//  IAM Policy Data
-// *****************************************/
-//
-//data "google_iam_policy" "limited_iam_binding_policy_data" {
-//
-//  binding {
-//    members = [
-//      "group:${var.limited_iam_bindings_group}",
-//    ]
-//    role = "roles/resourcemanager.projectIamAdmin"
-//
-//    condition {
-//      title       = "Test to limit IAM bindings permissions"
-//      expression  = "api.getAttribute('iam.googleapis.com/modifiedGrantsByRole', []).hasOnly(['roles/billing.admin', 'roles/billing.user', 'roles/bigquery.admin', 'roles/bigtable.admin'])"
-//    }
-//
-//  }
-//
-//
-//}
+#/**
+# *
+# * Licensed under the Apache License, Version 2.0 (the "License");
+# * you may not use this file except in compliance with the License.
+# * You may obtain a copy of the License at
+# *
+# *      http://www.apache.org/licenses/LICENSE-2.0
+# *
+# * Unless required by applicable law or agreed to in writing, software
+# * distributed under the License is distributed on an "AS IS" BASIS,
+# * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# * See the License for the specific language governing permissions and
+# * limitations under the License.
+# */
+#
+#/******************************************
+#  Project
+# *****************************************/
+#
+#resource "google_project" "limit_iam_binding_1" {
+#  name            = "${var.project_unique_shared_id}-limit-iam-binding"
+#  project_id      = "${var.project_unique_shared_id}-limit-iam-binding"
+#  folder_id       = google_folder.conditional_access_testing.id
+#  billing_account = var.billing_account_id
+#}
+#
+#resource "google_project" "limit_iam_binding_2" {
+#  name            = "${var.project_unique_shared_id}-limit-iam-binding"
+#  project_id      = "${var.project_unique_shared_id}-limit-iam-binding-2"
+#  folder_id       = google_folder.conditional_access_testing.id
+#  billing_account = var.billing_account_id
+#}
+#
+#
+#/******************************************
+#  IAM Policy
+# *****************************************/
+#
+#
+#resource "google_project_iam_member" "test_limited_iam_admin_permissions_1" {
+#  project = google_project.limit_iam_binding_1.project_id
+#  member = "group:${var.limited_iam_bindings_group}"
+#  role = "roles/resourcemanager.projectIamAdmin"
+#  condition {
+#    title       = "Test to limit IAM bindings permissions - First 130"
+#    expression  = "api.getAttribute('iam.googleapis.com/modifiedGrantsByRole', []).hasOnly(['roles/storage.admin', 'roles/bigquery.admin', 'roles/bigtable.admin', 'projects/iamtest-limit-iam-binding/roles/LotsORoles'])"
+#  }
+#}
+#
+#resource "google_project_iam_member" "test_limited_iam_admin_permissions_2" {
+#  project = google_project.limit_iam_binding_1.project_id
+#  member = "group:${var.limited_iam_bindings_group}"
+#  role = "roles/resourcemanager.projectIamAdmin"
+#  condition {
+#    title       = "Test to limit IAM bindings permissions - Second 130"
+#    expression  = "api.getAttribute('iam.googleapis.com/modifiedGrantsByRole', []).hasOnly(['roles/cloudbuild.builds.builder', 'roles/cloudbuild.builds.editor', 'roles/cloudbuild.builds.viewer'])"
+#  }
+#}
+#
+#
+#
+#
+#
+#
+#
+#//resource "google_folder_iam_binding" "test_limited_iam_admin_permissions" {
+#//    folder = google_folder.conditional_access_testing.id
+#//    members = [
+#//      "group:${var.limited_iam_bindings_group}",
+#//    ]
+#//    role = "roles/resourcemanager.projectIamAdmin"
+#//
+#//    condition {
+#//      title       = "Test to limit IAM bindings permissions"
+#//      expression  = "api.getAttribute('iam.googleapis.com/modifiedGrantsByRole', []).hasOnly(['roles/storage.admin', 'roles/billing.user', 'roles/bigquery.admin', 'roles/bigtable.admin']) || xyz " #Can OR it up to 12 times (13 functions to call)
+#//    }
+#//
+#//}
+#
+#
+#resource "google_service_account" "limited_iam_or_test" {
+#  project = google_project.limit_iam_binding_1.project_id
+#  account_id = "limited-iam-or-test"
+#}
+#
+#resource "google_folder_iam_binding" "test_limited_iam_admin_permissions" {
+#  folder = google_folder.conditional_access_testing.id
+#  members = [
+#    "group:${var.limited_iam_bindings_group}",
+#  ]
+#  role = "roles/resourcemanager.projectIamAdmin"
+#
+#  condition {
+#    title       = "Test to limit IAM bindings permissions"
+#    expression  ="api.getAttribute('iam.googleapis.com/modifiedGrantsByRole', []).hasOnly(['roles/storage.admin', 'roles/billing.user','roles/bigquery.admin', 'roles/bigtable.admin'])" #Can OR it up to 12 times (13 functions to call)
+#  }
+#
+#}
+#
+#//
+#//
+#///******************************************
+#//  IAM Policy Data
+#// *****************************************/
+#//
+#//data "google_iam_policy" "limited_iam_binding_policy_data" {
+#//
+#//  binding {
+#//    members = [
+#//      "group:${var.limited_iam_bindings_group}",
+#//    ]
+#//    role = "roles/resourcemanager.projectIamAdmin"
+#//
+#//    condition {
+#//      title       = "Test to limit IAM bindings permissions"
+#//      expression  = "api.getAttribute('iam.googleapis.com/modifiedGrantsByRole', []).hasOnly(['roles/billing.admin', 'roles/billing.user', 'roles/bigquery.admin', 'roles/bigtable.admin'])"
+#//    }
+#//
+#//  }
+#//
+#//
+#//}
