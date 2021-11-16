6d2b8941f600d868f3cecc7c346ad00a2e80c310
John Hurring Jr <jhurring@google.com>
2021-11-16 08:42:25 -0500

    Shutting Down

--- 01-shared-services/org-iam-policies-billing-sinks.tf
+++ 01-shared-services/org-iam-policies-billing-sinks.tf
@@ -41,101 +41,101 @@ module "org_policies" {
 }
 
 
-/******************************************
-  Org Policies - Folder - Trusted Image
- *****************************************/
-
-resource "google_folder_organization_policy" "define_trusted_image_project" {
-  constraint  = "constraints/compute.trustedImageProjects"
-  folder      = google_folder.business_trusted_image.id
-  list_policy {
-    allow {
-      values = ["projects/${module.os_images_project_prod.project_id}",]
-    }
-  }
-}
-
-/******************************************
-  Org - BigQuery
- *****************************************/
-
-module "org_sink_bigquery" {
-  source                          = "github.com/john-hurringjr/test-modules//org-sinks/bigquery?ref=master"
-  project_id                      = module.org_log_sink_project.project_id
-  bigquery_dataset_friendly_name  = "Org Sink"
-  bigquery_dataset_id             = var.bigquery_org_sink_data_set_id
-  sink_name                       = var.bigquery_org_sink_name
-  organization_id                 = var.organization_id
-}
-
-/******************************************
-  Org - GCS
- *****************************************/
-
-module "org_sink_gcs" {
-  source                       = "github.com/john-hurringjr/test-modules//org-sinks/gcs?ref=master"
-  project_id                   = module.org_log_sink_project.project_id
-  gcs_bucket_name              = var.gcs_org_sink_bucket_name
-  object_age_move_to_nearline  = 180
-  object_age_move_to_coldline  = 365
-  sink_name                    = var.gcs_org_sink_name
-  organization_id              = var.organization_id
-}
-
-/******************************************
-  Billing - PubSub
- *****************************************/
-
-module "billing_sink_pubsub" {
-  source                                 = "github.com/john-hurringjr/test-modules//billing-log-sink/pubsub?ref=master"
-  project_id                             = module.org_log_sink_project.project_id
-  sink_pubsub_topic_name                 = var.billing_sink_pubsub_topic_name
-  sink_pubsub_subscription_name          = var.billing_sink_pubsub_subscription_name
-  sink_name                              = var.billing_pubsub_sink_name
-  sink_subscription_service_account_name = var.billing_sink_subscription_service_account_name
-  billing_account                        = var.billing_account_id
-
-}
-
-/******************************************
-  Billing - GCS
- *****************************************/
-
-module "billing_sink_gcs" {
-  source                       = "github.com/john-hurringjr/test-modules//billing-log-sink/gcs?ref=master"
-  project_id                   = module.org_log_sink_project.project_id
-  gcs_bucket_name              = var.gcs_billing_sink_bucket_name
-  object_age_move_to_nearline  = 180
-  object_age_move_to_coldline  = 365
-  sink_name                    = var.gcs_billing_sink_name
-  billing_account              = var.billing_account_id
-}
-
-/******************************************
-  Billing 2 - PubSub
- *****************************************/
-
-//module "billing_2_sink_pubsub" {
-//  source                                 = "github.com/john-hurringjr/test-modules/billing-log-sink/pubsub"
-//  project_id                             = module.org_log_sink_project.project_id
-//  sink_pubsub_topic_name                 = var.billing_2_sink_pubsub_topic_name
-//  sink_pubsub_subscription_name          = var.billing_2_sink_pubsub_subscription_name
-//  sink_name                              = var.billing_2_pubsub_sink_name
-//  sink_subscription_service_account_name = var.billing_2_sink_subscription_service_account_name
-//  billing_account                        = var.billing_account_id_2
-//
-//}
-
-/******************************************
-  Billing 2 - GCS
- *****************************************/
-
-module "billing_2_sink_gcs" {
-  source                       = "github.com/john-hurringjr/test-modules//billing-log-sink/gcs?ref=master"
-  project_id                   = module.org_log_sink_project.project_id
-  gcs_bucket_name              = var.gcs_billing_2_sink_bucket_name
-  object_age_move_to_nearline  = 180
-  object_age_move_to_coldline  = 365
-  sink_name                    = var.gcs_billing_2_sink_name
-  billing_account              = var.billing_account_id_2
-}
+#/******************************************
+#  Org Policies - Folder - Trusted Image
+# *****************************************/
+#
+#resource "google_folder_organization_policy" "define_trusted_image_project" {
+#  constraint  = "constraints/compute.trustedImageProjects"
+#  folder      = google_folder.business_trusted_image.id
+#  list_policy {
+#    allow {
+#      values = ["projects/${module.os_images_project_prod.project_id}",]
+#    }
+#  }
+#}
+#
+#/******************************************
+#  Org - BigQuery
+# *****************************************/
+#
+#module "org_sink_bigquery" {
+#  source                          = "github.com/john-hurringjr/test-modules//org-sinks/bigquery?ref=master"
+#  project_id                      = module.org_log_sink_project.project_id
+#  bigquery_dataset_friendly_name  = "Org Sink"
+#  bigquery_dataset_id             = var.bigquery_org_sink_data_set_id
+#  sink_name                       = var.bigquery_org_sink_name
+#  organization_id                 = var.organization_id
+#}
+#
+#/******************************************
+#  Org - GCS
+# *****************************************/
+#
+#module "org_sink_gcs" {
+#  source                       = "github.com/john-hurringjr/test-modules//org-sinks/gcs?ref=master"
+#  project_id                   = module.org_log_sink_project.project_id
+#  gcs_bucket_name              = var.gcs_org_sink_bucket_name
+#  object_age_move_to_nearline  = 180
+#  object_age_move_to_coldline  = 365
+#  sink_name                    = var.gcs_org_sink_name
+#  organization_id              = var.organization_id
+#}
+#
+#/******************************************
+#  Billing - PubSub
+# *****************************************/
+#
+#module "billing_sink_pubsub" {
+#  source                                 = "github.com/john-hurringjr/test-modules//billing-log-sink/pubsub?ref=master"
+#  project_id                             = module.org_log_sink_project.project_id
+#  sink_pubsub_topic_name                 = var.billing_sink_pubsub_topic_name
+#  sink_pubsub_subscription_name          = var.billing_sink_pubsub_subscription_name
+#  sink_name                              = var.billing_pubsub_sink_name
+#  sink_subscription_service_account_name = var.billing_sink_subscription_service_account_name
+#  billing_account                        = var.billing_account_id
+#
+#}
+#
+#/******************************************
+#  Billing - GCS
+# *****************************************/
+#
+#module "billing_sink_gcs" {
+#  source                       = "github.com/john-hurringjr/test-modules//billing-log-sink/gcs?ref=master"
+#  project_id                   = module.org_log_sink_project.project_id
+#  gcs_bucket_name              = var.gcs_billing_sink_bucket_name
+#  object_age_move_to_nearline  = 180
+#  object_age_move_to_coldline  = 365
+#  sink_name                    = var.gcs_billing_sink_name
+#  billing_account              = var.billing_account_id
+#}
+#
+#/******************************************
+#  Billing 2 - PubSub
+# *****************************************/
+#
+#//module "billing_2_sink_pubsub" {
+#//  source                                 = "github.com/john-hurringjr/test-modules/billing-log-sink/pubsub"
+#//  project_id                             = module.org_log_sink_project.project_id
+#//  sink_pubsub_topic_name                 = var.billing_2_sink_pubsub_topic_name
+#//  sink_pubsub_subscription_name          = var.billing_2_sink_pubsub_subscription_name
+#//  sink_name                              = var.billing_2_pubsub_sink_name
+#//  sink_subscription_service_account_name = var.billing_2_sink_subscription_service_account_name
+#//  billing_account                        = var.billing_account_id_2
+#//
+#//}
+#
+#/******************************************
+#  Billing 2 - GCS
+# *****************************************/
+#
+#module "billing_2_sink_gcs" {
+#  source                       = "github.com/john-hurringjr/test-modules//billing-log-sink/gcs?ref=master"
+#  project_id                   = module.org_log_sink_project.project_id
+#  gcs_bucket_name              = var.gcs_billing_2_sink_bucket_name
+#  object_age_move_to_nearline  = 180
+#  object_age_move_to_coldline  = 365
+#  sink_name                    = var.gcs_billing_2_sink_name
+#  billing_account              = var.billing_account_id_2
+#}
